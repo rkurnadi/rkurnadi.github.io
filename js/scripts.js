c754d5e123c8f795ab1df24bc1750855d92cae87
rkurnadi <rkurnadi@gmail.com>
2017-05-30 20:42:51 -0700

    Select inputs

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,21 @@ $(document).ready(function() {
 	$.getJSON("./jsondata/data.json", function(json) {
 		myJson = json;
 		});
+		
+	var $select = $('#unitselect');
+	$.each(myJson, function(i, val){
+		$select.append($('<option />', { value: (i+1), text: val[i+1].name }));
+	});
+	
+	$select.change(function() {
+		var unit = $(this).val();
+		var $abilities = $("#abilityselect");
+		var index = $('option:selected',this).index();
+		var abilities = myJson[index].ability;
+		$.each(abilities, function(i, val){
+			$abilities.append($('<option />', { value: (i+1), text: val[i+1].name }));
+		});		
+	});
 });
 
 function saveData(cellId) {
